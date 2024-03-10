aedb2370dadc14d7384f40fbb5e422158023c55f
Ketan Baitule <111437670+ketanbaitule@users.noreply.github.com>
2024-03-10 10:32:13 +0530

    Add Profile Avatar and Details

--- src/components/profile/ProfileListView.jsx
+++ src/components/profile/ProfileListView.jsx
@@ -8,7 +8,31 @@ export default function ProfileListView({setActiveTab}){
     }
     return(
         <>
-            <BlockTitle large>Profile</BlockTitle>
+            <Block inset>
+                <div className="flex flex-col items-center gap-y-5 mt-5">
+                    <img className="rounded-full h-32" src="https://files.catbox.moe/tt6od5.webp" alt="user image" />
+                    <div className="text-xl font-semibold">
+                        Person Name
+                    </div>
+                </div>
+            </Block>
+
+            <Block inset strong>
+                <div className="flex flex-row divide-x-2">
+                    <div className="flex flex-col items-center gap-2 w-1/3 p-3">
+                        <div className="text-3xl">170 cm</div>
+                        <div className="font-semibold">Height</div>
+                    </div>
+                    <div className="flex flex-col items-center gap-2 w-1/3 p-3">
+                        <div className="text-3xl">50 kg</div>
+                        <div className="font-semibold">Weight</div>
+                    </div>
+                    <div className="flex flex-col items-center gap-2 w-1/3 p-3">
+                        <div className="text-3xl">A-</div>
+                        <div className="font-semibold">Blood Group</div>
+                    </div>
+                </div>
+            </Block>
 
             <BlockTitle medium>Emergency Contacts</BlockTitle>
 
