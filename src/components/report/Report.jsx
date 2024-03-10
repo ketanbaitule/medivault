d005a349dc470d3fc63a45d881143715e0bfebec
Ketan Baitule <111437670+ketanbaitule@users.noreply.github.com>
2024-03-10 09:39:32 +0530

    Add Report Page

--- src/components/report/Report.jsx
+++ src/components/report/Report.jsx
@@ -1,66 +1,56 @@
 import { Navbar, BlockTitle, List, ListItem, Tabbar, TabbarLink, Card } from "konsta/react";
-import MedicineTypeIcon from "@/components/shared/MedicineTypeIcon";
+import { TbReportMedical } from "react-icons/tb";
 import { MdNavigateNext } from "react-icons/md";
 
 export default function Report(){
-    const daily_review_medicines = [
+    const reports = [
         {
-          "name": "Oxycodon",
-          "time": "10:00 AM",
-          "type": "pills",
-          "status": "Completed",
+            "doc_id": 12,
+            "name": "MRI",
+            "hospital_name": "Unity Medical Center",
+            "date": "10/04/2023 10:00AM"
         },
         {
-          "name": "Nalaxon",
-          "time": "4:00 PM",
-          "type": "syrup",
-          "status": "Skipped",
+            "doc_id": 13,
+            "name": "CT Scan",
+            "hospital_name": "Serene Health Clinic",
+            "date": "10/04/2023 10:30AM"
         },
         {
-          "name": "Oxycodon",
-          "time": "10:00 AM",
-          "type": "pills",
-          "status": "Completed"
+            "doc_id": 14,
+            "name": "Blood Test",
+            "hospital_name": "Wellness Hospital",
+            "date": "10/04/2023 11:00AM"
         },
         {
-            "name": "Nalaxon",
-            "time": "4:00 PM",
-            "type": "syrup",
-            "status": "Skipped",
-        },
-        {
-            "name": "Nalaxon",
-            "time": "4:00 PM",
-            "type": "syrup",
-            "status": "Skipped",
-        },
+            "doc_id": 15,
+            "name": "Ultrasound",
+            "hospital_name": "Care Haven Medical Center",
+            "date": "10/04/2023 11:30AM"
+        }
       ]
+      
 
     return(
         <>
             <BlockTitle medium >Latest Report</BlockTitle>
             
-            <div className="my-4">
-                <Card>
-                    97
-                </Card>
-            </div>
             <List strong inset outline>
                 {
-                daily_review_medicines.map((medicine, index)=>{
+                reports.map((report, index)=>{
 
                     return(
                     <ListItem
                         key={index}
                         chevronMaterial={false}
-                        title={medicine.name}
+                        title={report.name}
                         footer={
                         <div className="w-full flex gap-x-2">
-                            <div>{medicine.time}</div>
-                            <div>{medicine.status}</div>
+                            <div>{report.hospital_name}</div>
+                            <div>{report.date}</div>
                         </div>
                         } 
-                        media={<MedicineTypeIcon type={medicine.type} /> }
+                        media={<TbReportMedical /> }
                         after={<MdNavigateNext className="h-full" />}
                     />
                     )
